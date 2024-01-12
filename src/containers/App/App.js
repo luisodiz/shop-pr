04a261893062a95f11b76f779b77b53e669da05c
Alexander Sosov <luisodiz@yandex.ru>
2024-01-12 13:57:56 +0300

    chore: добавить стартовые файлы

---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
